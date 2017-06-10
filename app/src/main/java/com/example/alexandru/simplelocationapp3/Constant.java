23b4da130cb3af668e52a9b9a11ff130db9cf4f0
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-06-10 17:45:58 +0300

    update 6

--- app/src/main/java/com/example/alexandru/simplelocationapp3/Constant.java
+++ app/src/main/java/com/example/alexandru/simplelocationapp3/Constant.java
@@ -6,12 +6,12 @@ package com.example.alexandru.simplelocationapp3;
 
 public final class Constant {
 
-    private Constant(){
-
-    }
-
     public static final String PACKAGE_NAME="com.example.alexandru.simplelocationapp3";
     public static final String BROADCAST_ACTION=PACKAGE_NAME+".BROADCAST_ACTION";
     public static final String ACTIVITY_EXTRA = PACKAGE_NAME+".ACTIVITY_EXTRA";
+
+    private Constant() {
+
+    }
 
 }
