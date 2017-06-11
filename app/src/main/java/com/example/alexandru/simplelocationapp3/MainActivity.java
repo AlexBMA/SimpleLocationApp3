25fda2e7b31900cfe17610592d5923ae6c6e5071
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-06-11 22:39:34 +0300

    update 12

--- app/src/main/java/com/example/alexandru/simplelocationapp3/MainActivity.java
+++ app/src/main/java/com/example/alexandru/simplelocationapp3/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.alexandru.simplelocationapp3;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -41,6 +45,26 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         buttonRemove = (Button) findViewById(R.id.remove_activity_updates_button);
         mBroadcastReceiver = new ActivityDetectionBroadcastReceiver();
         buildCreateApiClient();
+    }
+
+    public void requestActivityUpdatesButtonHandler(View view) {
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mGoogleApiClient, 5, getActivityDetectionPendingIntent()).
+                setResultCallback(this);
+
+        buttonRequest.setEnabled(false);
+        buttonRemove.setEnabled(true);
+    }
+
+    public void removeActivityUpdatesButtonHandler(View view) {
+        ActivityRecognition.ActivityRecognitionApi.removeActivityUpdates(mGoogleApiClient, getActivityDetectionPendingIntent())
+                .setResultCallback(this);
+
+        buttonRequest.setEnabled(true);
+        buttonRemove.setEnabled(false);
+    }
+
+    private PendingIntent getActivityDetectionPendingIntent() {
+        return null;
     }
 
 
@@ -97,12 +121,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     protected void onPause() {
+
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver);
         super.onPause();
     }
 
     @Override
     protected void onResume() {
+
         super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addCategory(Constant.BROADCAST_ACTION);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mBroadcastReceiver, intentFilter);
     }
 
 
