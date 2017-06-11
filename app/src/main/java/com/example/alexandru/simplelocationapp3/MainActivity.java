c6b2b413ca3fd24d9b4c004b3e8cbb0eb0d25cf6
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-06-11 18:54:27 +0300

    update 9

--- app/src/main/java/com/example/alexandru/simplelocationapp3/MainActivity.java
+++ app/src/main/java/com/example/alexandru/simplelocationapp3/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.alexandru.simplelocationapp3;
 
-import android.app.PendingIntent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -15,21 +17,26 @@ import com.google.android.gms.location.ActivityRecognitionResult;
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
 
+    protected final String TAG = "TAG";
     protected  GoogleApiClient mGoogleApiClient;
-
     protected  ActivityRecognitionResult mActivityRecognitionResult;
     protected  ActivityRecognition mActivityRecognition;
     protected  ActivityRecognitionApi mActivityRecognitionApi;
+    private TextView textViewDetected;
+    private Button buttonRequest;
+    private Button buttonRemove;
 
-
-
-    protected final  int I=1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewDetected = (TextView) findViewById(R.id.detectedActivities);
+        buttonRequest = (Button) findViewById(R.id.request_activity_updates_button);
+        buttonRemove = (Button) findViewById(R.id.remove_activity_updates_button);
+
         buildCreateApiClient();
     }
 
@@ -62,16 +69,22 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        Log.e(TAG, "connected");
+
 
     }
 
     @Override
     public void onConnectionSuspended(int i) {
+        Log.e(TAG, "connection Suspended ");
+        mGoogleApiClient.connect();
 
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG, "connection Failed");
+
 
     }
 }
